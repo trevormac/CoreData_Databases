59498b7061b4988ec16f6f8a1d72ffe49c2768a9
trevormac <trevor.c.macgregor@gmail.com>
2017-05-25 15:53:28 -0400

    Done. Can only add entities programatically.

--- Databases/ViewController.swift
+++ Databases/ViewController.swift
@@ -7,12 +7,48 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //declare constants for the names of our entities
+        let studentClassName:String = String(describing: Student.self)
+        let courseClassName:String =  String(describing: Course.self)
+        
+        //inserting this onto our managed object context
+        let student:Student = NSEntityDescription.insertNewObject(forEntityName: studentClassName, into: DatabaseController.getContext()) as! Student
+        student.firstName = "John"
+        student.lastName = "Smith"
+        student.age = 22
+        
+        let course:Course = NSEntityDescription.insertNewObject(forEntityName: courseClassName, into: DatabaseController.getContext()) as! Course
+        course.courseName = "Science"
+        
+        //connect the 2 entities
+        student.addToCourses(course)
+        
+        
+        DatabaseController.saveContext()
+        
+        let fetchRequest:NSFetchRequest<Student> = Student.fetchRequest()
+        
+        do {
+        let searchResults = try DatabaseController.getContext().fetch(fetchRequest)
+            print("number of results:\(searchResults.count)")
+            
+            for result in searchResults as [Student] {
+                print("\(result.firstName!) \(result.lastName!) is \(result.age) years old")
+            }
+        }
+        catch {
+            print("Error: \(error)")
+        }
+        DatabaseController.getContext().delete(student)
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
